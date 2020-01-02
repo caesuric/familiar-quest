6c9697324f6a48715efa0f49ccb26a5fed3b7234
Chris Muller <cmuller@cra.com>
2020-01-02 14:39:39 -0500

    misc. code cleanup

--- FamiliarQuestMainGame/Assets/Scripts/World/WorldAutoSaver.cs
+++ FamiliarQuestMainGame/Assets/Scripts/World/WorldAutoSaver.cs
@@ -1,12 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using UnityEngine;
-using UnityEngine.Networking;
 
 public class WorldAutoSaver : MonoBehaviour {
-
     private float autoSaveTimer = 0;
     public string worldName = "";
 
