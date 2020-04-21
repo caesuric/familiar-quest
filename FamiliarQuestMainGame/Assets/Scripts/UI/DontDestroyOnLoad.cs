98a62b7718ab13b06c0f585194a98f119e3f0600
caesuric <illusorycaesura@gmail.com>
2020-04-21 12:41:06 -0400

    replaced all DontDestroyOnLoad scripts with a unified version

---
+++ FamiliarQuestMainGame/Assets/Scripts/UI/DontDestroyOnLoad.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+    public static Dictionary<string, DontDestroyOnLoad> ddols = new Dictionary<string, DontDestroyOnLoad>();
+    public string type;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (!ddols.ContainsKey(type)) {
+            DontDestroyOnLoad(gameObject);
+            ddols[type] = this;
+        }
+        else Destroy(gameObject);
+    }
+}
