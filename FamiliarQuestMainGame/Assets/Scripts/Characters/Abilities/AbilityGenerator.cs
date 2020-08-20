6cb84b71ef1b56311ca51dc063c9edeed83afbef
caesuric <illusorycaesura@gmail.com>
2020-08-20 15:39:28 -0400

    refactored AbilityGenerator

---
+++ FamiliarQuestMainGame/Assets/Scripts/Characters/Abilities/AbilityGenerator.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+
+public static class AbilityGenerator {
+    public static Ability Generate(int level = 1) {
+        var roll = RNG.Int(0, 300);
+        if (roll < 100) return PassiveAbilityGenerator.Generate(level);
+        else if (roll < 273) return AttackAbilityGenerator.Generate(level);
+        else return UtilityAbilityGenerator.Generate(level);
+    }
+}
