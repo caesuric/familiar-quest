f0f528329249458f6d4db6c85a9aa5ac12a39df7
caesuric <illusorycaesura@gmail.com>
2023-09-17 16:35:14 -0400

    deleted some old AI files

--- FamiliarQuestMainGame/Assets/Scripts/AI_old/Goals/HurtPlayer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AI.Goals {
-    public class HurtPlayer : GoapGoal {
-        public HurtPlayer(float weight = 1) {
-            key = "playerHurt";
-            value = true;
-            this.weight = weight;
-        }
-    }
-}
--- FamiliarQuestMainGame/Assets/Scripts/AI_old/Goals/StayAlert.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AI.Goals {
-    public class StayAlert : GoapGoal {
-        public StayAlert(float weight = 1) {
-            key = "awareOfSurroundings";
-            value = true;
-            this.weight = weight;
-        }
-    }
-}
